5acc275c2874b0df8e0d2835e2f93c2aee4336dd
yry <761163902@qq.com>
2016-12-27 14:53:01 +0800

    添加开发依赖

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 const gulp     = require("gulp"),
       uglify   = require("gulp-uglify"),
-      cleanCss = require("gulp-clean-css");
+      cleanCss = require("gulp-clean-css"),
+      minHtml  = require("gulp-htmlmin"),
+      imgOptim = require("gulp-imageoptim");
 
+    //默认任务
+    gulp.task("default", ["minify-htm", "minify-css", "minify-js"]);
 
     //压缩js
     gulp.task("minify-js", function () {
@@ -17,4 +21,21 @@ const gulp     = require("gulp"),
                 compatibility: "ie9,+properties.zeroUnits"
             }))
             .pipe(gulp.dest("dist/css"));
+    })
+
+    //压缩html
+    gulp.task("minify-htm", function () {
+        gulp.src("app/*.html")
+            .pipe(minHtml({
+                collapseBooleanAttributes: true,
+                collapseInlineTagWhitespace: true
+            }))
+            .pipe(gulp.dest("dist"));
+    })
+
+    //压缩图片
+    gulp.task("minify-img", function () {
+        gulp.src("app/images/*")
+            .pipe(imgOptim.optimize())
+            .pipe(gulp.dest("dist/images"));
     })
